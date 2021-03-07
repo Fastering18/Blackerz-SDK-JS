fc31a20247a318124926c74c0780916582f1a7b9
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-03-07 09:13:03 +0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
 // Blackerz API SDK by Fastering18
 // More info join our Discord server
 
-const blackerz = require("blackers-api-sdk")
+const blackerz = require("./lib/main.js")
 
-botFunctions.Authorize("V1auth", "bot developer id");
+blackerz.botData("717660707650273280").then(console.log).catch(console.error)
+
+/*botFunctions.Authorize("V1auth");
 const goblox = new botFunctions.bot("bot id")
 
 goblox.edit({
@@ -12,4 +14,4 @@ goblox.edit({
     console.log(`${JSON.stringify(result)} sucess`)
 }).catch(console.error)
 
-goblox.botData().then(console.log).catch(console.error)
+goblox.botData().then(console.log).catch(console.error)*/
